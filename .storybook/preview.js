72789a75500b74403c1f26c2872754079d08d66f
Gerasin Ivan <webartmotor@gmail.com>
2020-03-08 11:18:25 +0300

    add Theme

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,5 +1,22 @@
 import { addParameters } from '@storybook/html';
 
+import '../src/components/Theme/Theme.post.css';
+import '../src/components/Theme/_font/Theme_font_default.post.css';
+import '../src/components/Theme/_color/Theme_color_default.post.css';
+import '../src/components/Theme/_size/Theme_size_default.post.css';
+
 addParameters({
-  themes: [],
+  themes: [
+    {
+      name: 'yandex',
+      class: [
+        'Theme',
+        'Theme_font_default',
+        'Theme_color_default',
+        'Theme_size_default',
+      ],
+      color: '#FFCC00',
+      default: true
+    },
+  ],
 });
---
+++ src/components/Theme/_color/color.stories.js
@@ -0,0 +1,7 @@
+import color from './color.html';
+
+export default {
+  title: 'Theme',
+}
+
+export const Color = () => color;

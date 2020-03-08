e4abb705b8855f76e1ae97550b1331ef1e3f8010
Gerasin Ivan <webartmotor@gmail.com>
2020-03-08 11:38:29 +0300

    add Icon component

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -5,6 +5,19 @@ import '../src/components/Theme/_font/Theme_font_default.post.css';
 import '../src/components/Theme/_color/Theme_color_default.post.css';
 import '../src/components/Theme/_size/Theme_size_default.post.css';
 
+// Icon
+import '../src/components/UI/Icon/Icon.post.css';
+import '../src/components/UI/Icon/_color/Icon_color_default.post.css';
+import '../src/components/UI/Icon/_color/Icon_color_success.post.css';
+import '../src/components/UI/Icon/_color/Icon_color_secondary.post.css';
+import '../src/components/UI/Icon/_color/Icon_color_warning.post.css';
+import '../src/components/UI/Icon/_color/Icon_color_error.post.css';
+import '../src/components/UI/Icon/_size/Icon_size_s.post.css';
+import '../src/components/UI/Icon/_size/Icon_size_m.post.css';
+import '../src/components/UI/Icon/_size/Icon_size_l.post.css';
+import '../src/components/UI/Icon/_size/Icon_size_xl.post.css';
+import '../src/components/UI/Icon/_size/Icon_size_7xl.post.css';
+
 addParameters({
   themes: [
     {
---
+++ src/components/UI/Icon/Icon.stories.js
@@ -0,0 +1,7 @@
+import icon from './Icon.html';
+
+export default {
+  title: 'UI'
+}
+
+export const Icon = () => icon;

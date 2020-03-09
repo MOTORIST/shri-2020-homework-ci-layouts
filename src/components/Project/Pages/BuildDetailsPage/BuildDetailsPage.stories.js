2170d1a1a7861962d460482b18a17f1d5501ee7e
Gerasin Ivan <webartmotor@gmail.com>
2020-03-09 17:11:06 +0300

    add BuildDetailsPage component

---
+++ src/components/Project/Pages/BuildDetailsPage/BuildDetailsPage.stories.js
@@ -0,0 +1,9 @@
+import buildDetailsPage from './BuildDetailsPage.html';
+
+// import '../../../global.post.css';
+
+export default {
+  title: 'Project/Pages',
+}
+
+export const BuildDetailsPage = () => buildDetailsPage;
